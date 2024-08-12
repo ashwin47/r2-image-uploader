f6d96c05922ba66efcb532f982d934e24b59522c
Ashwin M <thatsashwin@gmail.com>
2024-08-12 23:58:30 +0530

    Fix issues

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   before_action :set_image, only: %i[ show edit update destroy ]
 
   def index
-    @images = Image.all
+    @images = Image.all.order(created_at: :desc)
   end
 
   def show
@@ -20,6 +20,7 @@ class ImagesController < ApplicationController
 
     respond_to do |format|
       if @image.save
+        @image.set_url if @image.file.attached? && @image.url.blank?
         format.html { redirect_to image_url(@image), notice: "Image was successfully created." }
         format.json { render :show, status: :created, location: @image }
       else
@@ -32,6 +33,7 @@ class ImagesController < ApplicationController
   def update
     respond_to do |format|
       if @image.update(image_params)
+        @image.set_url if @image.file.attached? && @image.url.blank?
         format.html { redirect_to image_url(@image), notice: "Image was successfully updated." }
         format.json { render :show, status: :ok, location: @image }
       else
@@ -42,20 +44,24 @@ class ImagesController < ApplicationController
   end
 
   def destroy
+    @image = Image.find(params[:id])
     @image.destroy
-
     respond_to do |format|
       format.html { redirect_to images_url, notice: "Image was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
-  private
-    def set_image
-      @image = Image.find(params[:id])
-    end
 
-    def image_params
-      params.require(:image).permit(:name, :file)
-    end
+  private
+
+  def set_image
+    @image = Image.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to images_url, alert: "Image not found."
+  end
+
+  def image_params
+    params.require(:image).permit(:name, :file)
+  end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,14 @@
 class Image < ApplicationRecord
   has_one_attached :file
   validates :name, presence: true
+  validates :file, presence: true
 
   def set_url
-    update(url: rails_blob_path(file, only_path: true)) if file.attached?
+    if file.attached?
+      update(url: Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true))
+    end
+  rescue => e
+    Rails.logger.error "Failed to set URL for Image #{id}: #{e.message}"
+    update(url: nil)
   end
 end
